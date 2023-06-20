cf32ce2dd0ea795c93f56038a9357186085d314d
gorkavillar <gorka@gorkavillar.com>
2023-06-20 16:48:37 +0200

    Tema 03 - Terminando con clases dinámicas

---
+++ src/Main.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <input type="text" v-model="valorInput" />
+    <button @click="pasaPorConsola">Pasa por consola</button>
+    <p :class="claseTexto">{{ valorMayusculas }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, computed } from "vue";
+
+const valorInput = ref("");
+
+const pasaPorConsola = () => console.log(valorInput.value);
+
+const valorMayusculas = computed(() => valorInput.value.toUpperCase());
+
+const claseTexto = computed(() => {
+    if (valorInput.value.length <= 6) return "rojo"
+    return "verde"
+})
+</script>
+
+<style scoped>
+.rojo {
+    color: red;
+}
+.verde {
+    color: rgb(103, 218, 65);
+}
+</style>
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { createApp } from "vue"
+import Main from "./Main.vue"
+import "./style.css"
+
+createApp(Main).mount("div#app")
