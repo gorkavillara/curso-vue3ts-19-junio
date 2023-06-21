c7e428a1d660ae630328bea54cb341a01237449b
gorkavillar <gorka@gorkavillar.com>
2023-06-21 15:44:51 +0200

    Tema 05 - Terminando con las props

--- src/components/ListaTareas.vue
+++ src/components/ListaTareas.vue
@@ -1,10 +1,24 @@
 <template>
-    <h2>Esta es la lista de tareas</h2>
+  <MiSaludo nombre="Sandra" />
+  <h2>Esta es la lista de tareas</h2>
+  <div class="lista">
     <MiTarea titulo="Bajar la basura" />
     <MiTarea titulo="Sacar al perro" />
+  </div>
 </template>
 
 <script setup lang="ts">
 //@ts-ignore
-import MiTarea from "@components/MiTarea"
+import MiTarea from "@components/MiTarea";
+//@ts-ignore
+import MiSaludo from "@components/MiSaludo";
+const esDesarrollador = true
 </script>
+
+<style scoped>
+.lista {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+}
+</style>
---
+++ src/components/MiSaludo.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>Hola {{ nombre }}</h1>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  nombre: string;
+}>();
+// Puedo acceder a las props de forma programática
+</script>
