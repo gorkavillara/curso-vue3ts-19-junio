933986b1ff9888e043612de998ab8819875b7909
gorkavillar <gorka@gorkavillar.com>
2023-06-29 16:55:09 +0200

    ⌨️ Proyecto final v0

--- src/components/MecGame.vue
+++ src/components/MecGame.vue
@@ -1,15 +1,60 @@
 <template>
+  <div @keydown="escribeLetra">
+    <h2>Mec game</h2>
+    <div>{{ selectedPhrase }}</div>
     <div>
-        <h2>Mec game</h2>
-        <div>{{ selectedPhrase }}</div>
-        <div><input type="text" /></div>
-        <div>// Sección de ranking</div>
+      {{ enteredPhrase }}
     </div>
+    <div v-show="true">
+      <input type="text" value="" @keydown="escribeLetra" v-focus />
+    </div>
+    <button @click="resetEverything">Reset</button>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { phrases } from "../assets"
-import { ref } from "vue"
+import { ref, watch } from "vue"
 
-const selectedPhrase = ref(phrases[0])
+const initialTime = ref<Date | null>(null)
+
+const selectedPhrase = ref(phrases[1])
+const enteredPhrase = ref("")
+
+const vFocus = {
+  mounted (el: HTMLElement) {
+    el.focus()
+  }
+}
+
+const resetEverything = () => {
+  initialTime.value = null
+  enteredPhrase.value = ""
+  const num = Math.floor(Math.random() * 6)
+  const newPhrase = phrases[num]
+  selectedPhrase.value = newPhrase
+}
+
+const escribeLetra = (e: Event) => {
+  const ev = e as KeyboardEvent
+  const nextCharacter = ev.key
+  const position = enteredPhrase.value.length
+  const nextCorrectCharacter = selectedPhrase.value.charAt(position)
+
+  if (nextCharacter === nextCorrectCharacter) {
+    enteredPhrase.value = `${enteredPhrase.value}${nextCharacter}`
+  }
+}
+
+watch(enteredPhrase, (nextValue, prevValue) => {
+  if (enteredPhrase.value === selectedPhrase.value) {
+    const elapsedTime = new Date().getTime() - initialTime.value!.getTime()
+    alert(`Bien hecho! Has tardado ${elapsedTime / 1000} segundos`)
+  }
+
+  // Chequemos si empezamos a escribir correctamente
+  if (prevValue === "" && prevValue !== nextValue) {
+    initialTime.value = new Date()
+  }
+})
 </script>
--- src/views/LayoutView.vue
+++ src/views/LayoutView.vue
@@ -1,6 +1,91 @@
 <template>
-  <div>
-    <h1>Layout</h1>
-    <router-view></router-view>
+  <div class="view">
+    <div class="navbar">
+      <div class="container">
+        <a class="navbar-brand">
+          <span style="color: rgb(45, 173, 45)">Vue</span>nas tardes
+        </a>
+        <span>{{ titulo }}</span>
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link to="/" class="nav-link">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/students" class="nav-link">Students</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/game" class="nav-link">Game</router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="content">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
+
+<script setup lang="ts">
+const titulo = import.meta.env.VITE_APP_TITULO
+</script>
+
+<style scoped lang="scss">
+.view {
+  background-color: #f8f9fa;
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100vh;
+}
+
+.navbar {
+  background-color: #fff;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  padding: 10px 20px;
+  .container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+}
+
+.navbar-brand {
+  font-size: 24px;
+  font-weight: bold;
+  color: #333;
+  text-decoration: none;
+}
+
+.nav-item {
+  display: inline-block;
+  margin-left: 10px;
+}
+
+.nav-link {
+  color: #333;
+  text-decoration: none;
+}
+
+.content {
+  padding: 20px;
+}
+
+.card {
+  background-color: #fff;
+  border-radius: 5px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  padding: 20px;
+  margin-bottom: 20px;
+}
+
+.card-title {
+  font-size: 20px;
+  font-weight: bold;
+  margin-bottom: 10px;
+}
+
+.card-text {
+  color: #333;
+}
+</style>
